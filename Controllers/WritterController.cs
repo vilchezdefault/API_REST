8f8a0cf5a6289db8c48e0e6451a8649b9701c1a5
vilchezdefault <vilchezdefault08@gmail.com>
2025-01-10 00:44:11 +0000

    InsertPost

--- Controllers/WritterController.cs
+++ Controllers/WritterController.cs
@@ -24,12 +24,35 @@ namespace API_rest.Controllers
             var writer = WriterDataStore.Current.Writers.FirstOrDefault(x=> x.IdWriter_prop == writerId);
             if (writer == null)
             
-            return NotFound("Writer not found. :( )");
+                return NotFound("Writer not found. :( )");
+
             return Ok(writer);
             
         }
 
+        [HttpPost]
+        public ActionResult<PostInsertWriter> PostWriter(PostInsertWriter postInsertWriter)
+        {
+            var maxWriterId = WriterDataStore.Current.Writers.Max(x => x.IdWriter_prop);
 
+            var newWriter = new clWritter(){
+                
+                IdWriter_prop  = maxWriterId + 1,
+                Nickname_prop = postInsertWriter.Nickname_prop,
+                Style_prop = postInsertWriter.Style_prop,
+                Nationality_prop = postInsertWriter.Nationality_prop,
+                Crew_prop = postInsertWriter.Crew_prop
+            };
+
+
+            WriterDataStore.Current.Writers.Add(newWriter);
+
+            return CreatedAtAction(nameof(GetWriter),
+            new {writerId = newWriter.IdWriter_prop},
+            newWriter
+            );
+
+        }
 
     }
 }
--- Data/clWritter.cs
+++ Data/clWritter.cs
@@ -33,9 +33,47 @@ namespace API_rest.Data
             set { nationality = value; }
         }
 
+        private string crew;
+        public string Crew_prop
+        {
+            get { return crew; }
+            set { crew = value; }
+        }
+        
+
         public List<clWritter>? writers {get; set;}
- 
 
     }
+
+    public class PostInsertWriter
+    {
+        private string nickname;
+            public string Nickname_prop
+            {
+                get { return nickname; }
+                set { nickname = value; }
+            }
+            private string style;
+            public string Style_prop
+            {
+                get { return style; }
+                set { style = value; }
+            }
+            private string nationality;
+            public string Nationality_prop
+            {
+                get { return nationality; }
+                set { nationality = value; }
+            }
+
+            private string crew;
+            public string Crew_prop
+            {
+                get { return crew; }
+                set { crew = value; }
+            }
+            
+    }
+
     
 }
