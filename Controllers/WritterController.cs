04ff7d163371ab8cef18a2049891d0e2cea8a4fe
vilchezdefault <vilchezdefault08@gmail.com>
2025-01-13 15:10:58 +0000

    Es hoy

--- Controllers/WritterController.cs
+++ Controllers/WritterController.cs
@@ -1,8 +1,6 @@
-using Microsoft.Data.SqlClient;  // Asegúrate de estar usando Microsoft.Data.SqlClient
 using Microsoft.AspNetCore.Mvc;
 using API_rest.Data;
-using System.Data;
-using System.Collections.Generic;
+
 using API_rest.services;
 
 namespace API_rest.Controllers
